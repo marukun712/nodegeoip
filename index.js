39301f0b105b0d0df0d2c9b53ad0e836eb8130e3
yutakobayashidev <yutakobayashi0924@gmail.com>
2022-06-13 18:44:08 +0900

    add: emoji

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 const iplocate = require("node-iplocate");
-const readline = require('readline');
-const asciify = require('asciify');
+const readline = require("readline");
+const asciify = require("asciify");
+const countryFlagEmoji = require("country-flag-emoji");
 
 const readInterface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
-readInterface.question("ipアドレスを入力してください >",
-    inputString => {
-        readInterface.close();
-        console.log(`入力されたip [${inputString }]`);
-        var ip = inputString
-        asciify("result", { font: 'larry3d' }, function(err, aa) {
-            console.log(aa);
-        });
-        iplocate(ip).then(function(results) {
-            console.log('---------------------------------------------------------------------')
-            console.log("IP Address: " + results.ip);
-            console.log("Country: " + results.country + " (" + results.country_code + ")");
-            console.log("Continent: " + results.continent);
-            console.log("Organisation: " + results.org + " (" + results.asn + ")");
-            console.log(JSON.stringify(results, null, 2));
-            console.log(`Googlemap https://www.google.com/maps?q=${results.latitude},${results.longitude}`)
-            console.log('---------------------------------------------------------------------')
+readInterface.question("ipアドレスを入力してください >", (inputString) => {
+  readInterface.close();
+  console.log(`入力されたip [${inputString}]`);
+  var ip = inputString;
+  asciify("result", { font: "larry3d" }, function (err, aa) {
+    console.log(aa);
+  });
 
-        });
-    });
+  iplocate(ip).then(function (results) {
+    const emoji = countryFlagEmoji.get(results.country_code);
+
+    console.log(
+      "---------------------------------------------------------------------"
+    );
+    console.log("IP Address: " + results.ip);
+    console.log(
+      "Country: " + results.country + " (" + results.country_code + ")"
+    );
+    console.log("Country Emoji: " + emoji.emoji + " (" + emoji.unicode + ")");
+    console.log("Continent: " + results.continent);
+    console.log("Organisation: " + results.org + " (" + results.asn + ")");
+    console.log(JSON.stringify(results, null, 2));
+    console.log(
+      `Googlemap https://www.google.com/maps?q=${results.latitude},${results.longitude}`
+    );
+    console.log(
+      "---------------------------------------------------------------------"
+    );
+  });
+});
